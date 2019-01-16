b69dceec6ffdbfdbbba9b4bf81da0abe9f643332
Kevin <kangl010@fiu.edu>
2019-01-16 13:08:41 -0500

    Let auth service handle routing instead of routerLink

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
   path = 'http://localhost:3000';
   TOKEN_KEY = 'token';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   get token() {
     return localStorage.getItem(this.TOKEN_KEY);
@@ -23,12 +24,14 @@ export class AuthService {
   registerUser(registerData) {
     this.http.post<any>(this.path + '/register', registerData).subscribe(res => {
       this.saveToken(res.token);
+      this.router.navigate(['/home']);
     });
   }
 
   loginUser(loginData) {
     this.http.post<any>(this.path + '/login', loginData).subscribe(res => {
       this.saveToken(res.token);
+      this.router.navigate(['/home']);
     });
   }
 }

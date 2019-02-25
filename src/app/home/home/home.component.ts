169b892e344a40e01c30ed76d38c5ef981f2986f
Kevin <kangl010@fiu.edu>
2019-02-24 22:00:12 -0500

    Remove title from home component when on mobile

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '.././home.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { HomeService } from '.././home.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  navTitle = 'Search';
 
-  constructor(private homeService: HomeService) { }
+  constructor(private homeService: HomeService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.setNavTitle(this.navTitle);
   }
 }

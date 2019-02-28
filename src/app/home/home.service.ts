e4fd470434f2427e81f8b40f26461d87148e6a71
Kevin <kangl010@fiu.edu>
2019-02-28 16:52:19 -0500

    Implement searchByCode call

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,27 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class HomeService {
   path = environment.path;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   verifyEmail(hash): Observable<any> {
     return this.http.post<any>(this.path + '/verify/' + hash, hash);
-  }
-
-  getCourses() {
-    return this.http.get(this.path + '/search');
   }
 
   getProfile(id) {
     return this.http.get(this.path + '/profile/' + id);
   }
 
-  getResults(code) {
-    return this.http.get(this.path + '/results/' + code);
+  searchByCode(code: string): Observable<any> {
+    return this.http.get(this.path + '/search/' + code);
   }
 }

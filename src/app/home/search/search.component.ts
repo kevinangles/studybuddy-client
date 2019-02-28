f4b3e3af1ef22e20a7dc450590174d090d0c8203
Kevin <kangl010@fiu.edu>
2019-02-28 16:47:03 -0500

    Add search functionality to search bar

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '.././home.service';
 import { AuthService } from '../../auth/auth.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -8,12 +10,21 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
   navTitle = 'Search';
 
-  constructor(private homeService: HomeService, private authService: AuthService) { }
+  constructor(private fb: FormBuilder, private homeService: HomeService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.searchForm = this.fb.group({
+      code: '',
+    });
     this.authService.setNavTitle(this.navTitle);
+  }
+
+  post() {
+    this.router.navigate(['/results/', this.searchForm.value.code])
+    // this.homeService.searchByCode(this.searchForm.value.code);
   }
 
 }

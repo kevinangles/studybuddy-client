3b4f8b34890ede91d691bdbd85d94f9c59c11457
Kevin <kangl010@fiu.edu>
2019-03-05 19:01:40 -0500

    Add filter by professor and reference functionality on results page

--- src/app/home/results/results.component.ts
+++ src/app/home/results/results.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home.service';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-results',
@@ -9,22 +10,73 @@ import { AuthService } from '../../auth/auth.service';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
-  courses;
-  courseName;
-  navTitle;
+  courses: any[] = [];
+  filteredCourses: any[] = [];
+  courseCode: string;
+  courseName: string;
+  professors: string[] = [];
+  references: number[] = [];
+  query: string;
 
   constructor(private homeService: HomeService, private route: ActivatedRoute, private authService: AuthService) {
-    
+
   }
 
   ngOnInit() {
-    this.courses = this.homeService.searchByCode(this.route.snapshot.params.code).subscribe((data) => {
+    this.courseCode = this.route.snapshot.params.code;
+    this.authService.setNavTitle(this.courseCode);
+
+    this.homeService.searchByCode(this.courseCode).subscribe((data) => {
       this.courses = data;
-      this.courseName = data[0].name;
-      this.navTitle = data[0].code
-      this.authService.setNavTitle(this.navTitle);
-    });;
-    
+      this.filteredCourses = data;
+      this.courseName = this.courses[0].name;
+      this.professors = this.getProfessorNames(this.courses);
+      this.references = this.getReferences(this.courses);
+    });
   }
 
+  getProfessorNames(courses: any[]) {
+    let names: string[] = [];
+    let uniqueNames: string[] = [];
+
+    names = courses.map(course => course.professor.name);
+
+    uniqueNames = names.filter((item, i, array) => {
+      return array.indexOf(item) === i;
+    });
+
+    return uniqueNames;
+  }
+
+  getReferences(courses: any[]) {
+    let references: number[] = [];
+
+    references = courses.map(course => course.reference);
+
+    return references;
+  }
+
+  filterByProfessor(professor: string) {
+    this.query = '';
+    const filter = professor.toLocaleLowerCase();
+    if ($(`input:checkbox[name='${professor}']`).is(":checked")) {
+      this.filteredCourses = this.courses.filter((course) =>
+        course.professor.name.toLocaleLowerCase().indexOf(filter) !== -1);
+      this.query = professor;
+    } else {
+      this.filteredCourses = this.courses;
+    }
+  }
+
+  filterByReference(reference: string) {
+    this.query = '';
+    const filter = reference.toLocaleLowerCase();
+    if ($(`input:checkbox[name='${reference}']`).is(":checked")) {
+      this.filteredCourses = this.courses.filter((course) =>
+        course.reference.toLocaleLowerCase().indexOf(filter) !== -1);
+      this.query = reference;
+    } else {
+      this.filteredCourses = this.courses;
+    }
+  }
 }
